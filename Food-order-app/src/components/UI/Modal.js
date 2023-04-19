ef53fbcfc9d7cff31cf626abf1dde2468dc441b2
Filip Sieradzki <sieradzki.filip@gmail.com>
2023-04-19 22:37:03 +0200

    forged ui

---
+++ Food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,25 @@
+import classes from './Modal.module.css'
+import { createPortal } from 'react-dom';
+
+function Backdrop() {
+ return <div className={classes.backdrop} />
+}
+
+function ModalOverlay(props) {
+    return (<div className={classes.modal}>
+            <div className={classes.content}>
+                {props.children}
+            </div>
+        </div>)
+}
+
+const portalElement = document.getElementById('overlays');
+
+export default function Modal(props) {
+    return (
+        <>
+        {createPortal(<Backdrop />, portalElement)}
+        {createPortal(<ModalOverlay> {props.children} </ModalOverlay>, portalElement)}
+        </>
+    )
+}
