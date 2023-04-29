9eb44b44864df9f3125f45cbe5279b3c8e3ecd91
Filip Sieradzki <sieradzki.filip@gmail.com>
2023-04-30 00:18:54 +0200

    finished food-order-app

--- Food-order-app/src/components/Layout/HeaderCartButton.js
+++ Food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
 
 export default function HeaderCartButton(props) {
-
+    const [btnIsHiglighted, setbtnIsHiglighted] = useState(false)
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const { items } = cartCtx
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0)
 
-   return <button className={classes.button} onClick={props.onClick}>
+
+    const btnClasses = `${classes.button} ${ btnIsHiglighted ? classes.bump : ''}`
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setbtnIsHiglighted(true)
+
+        const timer = setTimeout(() => {setbtnIsHiglighted(false)},300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+
+    },[items])
+
+   return <button className={btnClasses} onClick={props.onClick}>
     <span className={classes.icon}>
         <CartIcon />
     </span>
